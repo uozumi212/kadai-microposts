1594414a2086f041aac1afcc8b93874dfbc40815
kazuma.uozumi <kazu3278mana@gmail.com>
2021-09-13 09:13:50 +0000

    top page

--- config/initializers/generators.rb
+++ config/initializers/generators.rb
@@ -1,6 +1,6 @@
-Rails.application.config.generatous do |g|
-    g.stylesheets false
-    g.javascripts false
-    g.helper false
-    g.skip_routes true
+Rails.application.config.generators do |g|
+  g.stylesheets false
+  g.javascripts false
+  g.helper false
+  g.skip_routes true
 end
