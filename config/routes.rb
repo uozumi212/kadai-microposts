2cc4b9f27ea762bf4319438c7506504ac9edbeba
kazuma.uozumi <kazu3278mana@gmail.com>
2021-09-14 06:04:36 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   root to: 'toppages#index'
+  
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destoroy"
+  
+  get "signup", to: "users#new"
+  resources :users, only: [:index, :show, :create]
 end
