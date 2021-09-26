f544117d1f43ce8e2814a93461ba9bf11132f5aa
kazuma.uozumi <kazu3278mana@gmail.com>
2021-09-26 07:32:35 +0000

    follow unfollow

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,8 @@ class ApplicationController < ActionController::Base
     end
     
     def counts(user)
-        @count_microposts = user.microposts.count_microposts
+        @count_microposts = user.microposts.count
+        @count_followings = user.followings.count
+        @count_followers = user.followers.count
     end
 end
